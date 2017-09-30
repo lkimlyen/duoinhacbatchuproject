9b5138bb77c11930f0126777ff197b04127a1789
yenyen-PC\yenyen <lkimlyen2912@gmail.com>
2017-09-30 12:07:52 +0700

    up code lên lần đầu

---
+++ MyApplication/app/src/main/java/com/duoihinhbatchu/demo/app/yenyen/duoihinhbatchuapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.duoihinhbatchu.demo.app.yenyen.duoihinhbatchuapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
