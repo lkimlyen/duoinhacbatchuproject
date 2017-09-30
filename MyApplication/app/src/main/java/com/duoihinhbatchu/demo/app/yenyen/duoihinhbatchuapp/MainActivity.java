a79ee32d50995e50b05721c4652a6b6d54c881fa
yenyen-PC\yenyen <lkimlyen2912@gmail.com>
2017-09-30 16:24:10 +0700

    add play button

--- MyApplication/app/src/main/java/com/duoihinhbatchu/demo/app/yenyen/duoihinhbatchuapp/MainActivity.java
+++ MyApplication/app/src/main/java/com/duoihinhbatchu/demo/app/yenyen/duoihinhbatchuapp/MainActivity.java
@@ -3,7 +3,10 @@ package com.duoihinhbatchu.demo.app.yenyen.duoihinhbatchuapp;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
 import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
         anim.setDuration(15000);
         anim.setInterpolator(new DecelerateInterpolator());
         anim.start();
+
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        rotateAnimation.setDuration(1500);
+        rotateAnimation.setRepeatCount(Animation.INFINITE);
+        findViewById(R.id.imageView1).startAnimation(rotateAnimation);
     }
 }
