b450f42c98ec497e5e519cbb58fe53a35d48323a
yenyen-PC\yenyen <lkimlyen2912@gmail.com>
2017-09-30 14:09:03 +0700

    add progress bar

--- MyApplication/app/src/main/java/com/duoihinhbatchu/demo/app/yenyen/duoihinhbatchuapp/MainActivity.java
+++ MyApplication/app/src/main/java/com/duoihinhbatchu/demo/app/yenyen/duoihinhbatchuapp/MainActivity.java
@@ -1,13 +1,22 @@
 package com.duoihinhbatchu.demo.app.yenyen.duoihinhbatchuapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    ProgressBar mprogressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mprogressBar = (ProgressBar) findViewById(R.id.circular_progress_bar);
+        ObjectAnimator anim = ObjectAnimator.ofInt(mprogressBar, "progress", 0, 100);
+        anim.setDuration(15000);
+        anim.setInterpolator(new DecelerateInterpolator());
+        anim.start();
     }
 }
